2e3102e36dea1fa99b3f85f564e43ee03ea7c7fb
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-01 20:24:58 +0530

    Create practice.c

---
+++ practice.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main(){
+  int a,b;
+  printf("Enter the length of the rectangle :");
+  scanf("%d",&a);
+  printf("Enter the bredth of the rectangle :");
+  scanf("%d",&b);
+
+  printf("the area of the rectangle is : %d",a*b);
+return 0;
+}
