9c799255e3944de61e2b7f78adf8319451430a7a
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-01 20:29:32 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -10,3 +10,18 @@ int main(){
   printf("factorial of your number is = %d",fact);
   return 0;
 }
+
+// even or odd
+#include<stdio.h>
+int main(){
+  int a;
+  printf("enter a number :");
+  scanf("%d",&a);
+  if (a%2 == 0){
+    printf("EVEN");
+  }
+  else{
+    printf("ODD");
+  }
+  return 0;
+}
