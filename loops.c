829ad76cc69a68faba81b4b6b203d8fb0dc5757e
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-04 17:44:56 +0530

    sum of all natural number

--- loops.c
+++ loops.c
@@ -1,7 +1,19 @@
+// #include<stdio.h>
+//     int main(){
+//     for(int i=1;i<=10;i++){
+//         printf("%d\n",i);
+//     }
+// return 0;
+//     }
+
 #include<stdio.h>
     int main(){
-    for(int i=1;i<=10;i++){
-        printf("%d\n",i);
+        int n,sum = 0;
+        printf("Enter a number :");
+        scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        sum = sum+i;
     }
+    printf("The total sum is : %d\n",sum);
 return 0;
 }
