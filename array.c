0ee694e134fa1dbb3e56bdbfb9624ee240409f45
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-01 17:17:36 +0530

    taking user input to print the elements of array

--- array.c
+++ array.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
     int main(){
-    int a[5] = {20,21,22,34,23};
+    int a[5];
+    for (int i = 0; i<=4 ; i++){
+        printf("Enter %d number :",i+1);
+        scanf("%d",&a[i]);
+        }
 
-    for(int i = 0;i<5 ; i++){
-        printf("%d\n",a[i]);
+    for(int j = 0 ;j<=4 ; j++){
+        printf("%d \n",a[j]);
     }
 return 0;
 }
