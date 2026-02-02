62a7695b398b053e3828aef163dd436697f10443
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-02 21:51:30 +0530

    commenting all

--- problem_array.c
+++ problem_array.c
@@ -35,17 +35,17 @@
 //     return 0;
 // }
 
-// Calculate the sum of all elements
-#include <stdio.h>
+// // Calculate the sum of all elements
+// #include <stdio.h>
 
-int main() {
-    int a[5], sum = 0;
+// int main() {
+//     int a[5], sum = 0;
 
-    for(int i = 0; i < 5; i++) {
-        scanf("%d", &a[i]);
-        sum += a[i];
-    }
+//     for(int i = 0; i < 5; i++) {
+//         scanf("%d", &a[i]);
+//         sum += a[i];
+//     }
 
-    printf("Sum = %d", sum);
-    return 0;
-}
+//     printf("Sum = %d", sum);
+//     return 0;
+// }
