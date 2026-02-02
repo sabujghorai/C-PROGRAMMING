d88b16432c847fa84c822190a0039122bda2db51
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-02 21:50:57 +0530

    calculate the sum of all elements

--- problem_array.c
+++ problem_array.c
@@ -34,3 +34,18 @@
 //     }
 //     return 0;
 // }
+
+// Calculate the sum of all elements
+#include <stdio.h>
+
+int main() {
+    int a[5], sum = 0;
+
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
