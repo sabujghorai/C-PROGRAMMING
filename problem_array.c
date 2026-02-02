208e3bb5804f857966ec46db7a89682fad93981d
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-02 21:49:05 +0530

    read n elements and print them

--- problem_array.c
+++ problem_array.c
@@ -1,17 +1,36 @@
-// Take 5 input from user and print them in reverse....
+// // Take 5 input from user and print them in reverse....
 
-#include<stdio.h>
-    int main(){
-    int arr[5];
+// #include<stdio.h>
+//     int main(){
+//     int arr[5];
 
-    for(int i = 0; i<5; i++){
-        printf("Enter element no %d :",i+1);
-        scanf("%d",&arr[i]);
+//     for(int i = 0; i<5; i++){
+//         printf("Enter element no %d :",i+1);
+//         scanf("%d",&arr[i]);
+//     }
+
+//     for(int j=4 ; j>=0 ; j--){
+//         printf("%d \n",arr[j]);
+//     }
+// return 0;
+// }
+
+// Read n elements and print them
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
 
-    for(int j=4 ; j>=0 ; j--){
-        printf("%d \n",arr[j]);
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
     }
-return 0;
+    return 0;
 }
-
