00c10992d1e45409dc5edc4d01994039122f431c
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-05 21:57:58 +0530

    printing the roll number of students of below 35

--- problem_array.c
+++ problem_array.c
@@ -1,16 +1,32 @@
 // Take 5 input from user and print them in reverse....
 
+// #include<stdio.h>
+//     int main(){
+//     int arr[5];
+
+//     for(int i = 0; i<5; i++){
+//         printf("Enter element no %d :",i+1);
+//         scanf("%d",&arr[i]);
+//     }
+
+//     for(int j=4 ; j>=0 ; j--){
+//         printf("%d \n",arr[j]);
+//     }
+// return 0;
+// }
+
+
+/*  Given an array of marks of students. if the mark of student is less than 35
+print it's roll number [Roll numbers here refers to the index of the array ]   */  
+
 #include<stdio.h>
     int main(){
-    int arr[5];
+    int marks[10] = {95,90,31,25,100,50,65,89,97,30};
 
-    for(int i = 0; i<5; i++){
-        printf("Enter element no %d :",i+1);
-        scanf("%d",&arr[i]);
-    }
-
-    for(int j=4 ; j>=0 ; j--){
-        printf("%d \n",arr[j]);
+    for(int i=1; i<10 ;i++){
+        if (marks[i]<35)
+        {printf(" Roll number is : %d \n",i);
+        }
     }
 return 0;
 }
