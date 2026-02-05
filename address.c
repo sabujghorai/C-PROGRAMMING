4c730de7990f0c6de1f2687f1de48247299f4e68
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-05 22:53:38 +0530

    printing the adress of the array

---
+++ address.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main(){
+    int arr[5] = {4,5,6,7,8};
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[1]);
+    printf("%p\n",&arr[2]);
+    printf("%p\n",&arr[3]);
+    printf("%p\n",&arr[4]);
+return 0;
+}
