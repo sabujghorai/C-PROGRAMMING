dcd5a6cc23a7423fb35979e5b5b6f4fd29a156be
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-07 23:00:42 +0530

    print to read n elements into an array

--- userInput_Array.c
+++ userInput_Array.c
@@ -12,3 +12,24 @@
 //         printf("%d ",arr[i]);
 //     }
 // return 0;
+
+// Write a C program to read n elements into an array and print them.
+
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
