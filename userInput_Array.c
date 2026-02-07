190498afeee3f1529c01a818e64a42d03080eb55
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-07 23:04:01 +0530

    printing the sum of array

--- userInput_Array.c
+++ userInput_Array.c
@@ -33,3 +33,22 @@
 
 //     return 0;
 // }
+
+// Find the sum of all elements in an array.
+
+#include <stdio.h>
+
+int main() {
+    int n, arr[100], sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
