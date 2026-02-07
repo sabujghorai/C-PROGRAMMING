2bbd3c9a2ea1f7c661a42e36f791843dfbec2e9d
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-07 22:59:15 +0530

    commentin all

--- userInput_Array.c
+++ userInput_Array.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
-    int main(){
-    int n;
-    printf("Enter the length of the array : ");
-    scanf("%d",&n);
+// #include<stdio.h>
+//     int main(){
+//     int n;
+//     printf("Enter the length of the array : ");
+//     scanf("%d",&n);
 
-    int arr[n];
-    for(int i=0 ;i<=n-1 ;i++){
-        scanf("%d",&arr[i]);
-    }
-    for(int i=0 ;i<=n-1; i++){
-        printf("%d ",arr[i]);
-    }
-return 0;
-}
+//     int arr[n];
+//     for(int i=0 ;i<=n-1 ;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i=0 ;i<=n-1; i++){
+//         printf("%d ",arr[i]);
+//     }
+// return 0;
