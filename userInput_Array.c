db6aa591a0fea67ad5011c28150f27bf42d237c5
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-07 23:05:34 +0530

    clear the block of codes compying from chatgpt

--- userInput_Array.c
+++ userInput_Array.c
@@ -1,54 +1,15 @@
-// #include<stdio.h>
-//     int main(){
-//     int n;
-//     printf("Enter the length of the array : ");
-//     scanf("%d",&n);
+#include<stdio.h>
+    int main(){
+    int n;
+    printf("Enter the length of the array : ");
+    scanf("%d",&n);
 
-//     int arr[n];
-//     for(int i=0 ;i<=n-1 ;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     for(int i=0 ;i<=n-1; i++){
-//         printf("%d ",arr[i]);
-//     }
-// return 0;
-
-// // Write a C program to read n elements into an array and print them.
-
-// #include <stdio.h>
-
-// int main() {
-//     int n, arr[100];
-
-//     printf("Enter number of elements: ");
-//     scanf("%d", &n);
-
-//     for(int i = 0; i < n; i++) {
-//         scanf("%d", &arr[i]);
-//     }
-
-//     for(int i = 0; i < n; i++) {
-//         printf("%d ", arr[i]);
-//     }
-
-//     return 0;
-// }
-
-// Find the sum of all elements in an array.
-
-#include <stdio.h>
-
-int main() {
-    int n, arr[100], sum = 0;
-
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
+    int arr[n];
+    for(int i=0 ;i<=n-1 ;i++){
+        scanf("%d",&arr[i]);
     }
-
-    printf("Sum = %d", sum);
-    return 0;
+    for(int i=0 ;i<=n-1; i++){
+        printf("%d ",arr[i]);
+    }
+return 0;
 }
