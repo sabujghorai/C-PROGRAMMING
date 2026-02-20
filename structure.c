003d89df7c342e21661f8e441908babadfd2e7d2
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-20 22:16:45 +0530

    making a structure of Student

--- structure.c
+++ structure.c
@@ -1,5 +1,23 @@
-#include<stdio.h>
-    int main(){
-    
-return 0;
+#include <stdio.h>
+
+// Define structure
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s1;
+
+    // Assign values
+    s1.roll = 101;
+    strcpy(s1.name, "Sabuj");
+    s1.marks = 88.5;
+
+    // Display values
+    printf("Roll: %d\n", s1.roll);
+    printf("Name: %s\n", s1.name);
+    printf("Marks: %.2f\n", s1.marks);
+    return 0;
 }
