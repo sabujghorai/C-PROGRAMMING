84ef22a9d53bbf8d3810322003d766fe6accba32
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-20 22:21:16 +0530

    making a structure of array

--- array_structure.c
+++ array_structure.c
@@ -1,5 +1,22 @@
-#include<stdio.h>
-    int main(){
-    
-return 0;
+#include <stdio.h>
+
+struct Student {
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s[3];
+
+    for(int i = 0; i < 3; i++) {
+        printf("Enter roll and marks for student %d: ", i+1);
+        scanf("%d %f", &s[i].roll, &s[i].marks);
+    }
+
+    printf("\nStudent Details:\n");
+    for(int i = 0; i < 3; i++) {
+        printf("Roll: %d, Marks: %.2f\n", s[i].roll, s[i].marks);
+    }
+
+    return 0;
 }
