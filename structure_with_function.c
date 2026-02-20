578eaa51dfc9851f5dd09f9d0256a58a873a0893
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-20 22:19:07 +0530

    making a structure with function

--- structure_with_function.c
+++ structure_with_function.c
@@ -1,5 +1,21 @@
-#include<stdio.h>
-    int main(){
-    
-return 0;
+#include <stdio.h>
+
+struct Rectangle {
+    int length;
+    int width;
+};
+
+int area(struct Rectangle r) {
+    return r.length * r.width;
+}
+
+int main() {
+    struct Rectangle rect;
+
+    rect.length = 10;
+    rect.width = 5;
+
+    printf("Area of Rectangle: %d\n", area(rect));
+
+    return 0;
 }
