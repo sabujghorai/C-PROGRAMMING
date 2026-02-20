6803db3220c077b81fe23cf1ab69531f3a5c257d
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-20 22:24:07 +0530

    writing a code for stack using array

--- stack.c
+++ stack.c
@@ -1,5 +1,70 @@
-#include<stdio.h>
-    int main(){
-    
-return 0;
+#include <stdio.h>
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("Deleted element: %d\n", stack[top--]);
+}
+
+void peek() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Top element: %d\n", stack[top]);
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+        return;
+    }
+    for (int i = top; i >= 0; i--) {
+        printf("%d\n", stack[i]);
+    }
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n1.Push  2.Pop  3.Peek  4.Display  5.Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 }
