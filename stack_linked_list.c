d71dd9f596c298945f1585a9a15eb9605b84bf51
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-20 22:26:16 +0530

    writing a stack code using linked list

--- stack_linked_list.c
+++ stack_linked_list.c
@@ -1,5 +1,50 @@
-#include<stdio.h>
-    int main(){
-    
-return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    struct Node* temp = top;
+    printf("Deleted element: %d\n", temp->data);
+    top = temp->next;
+    free(temp);
+}
+
+void display() {
+    struct Node* temp = top;
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    printf("Stack elements:\n");
+    display();
+
+    pop();
+    printf("After pop:\n");
+    display();
+
+    return 0;
 }
