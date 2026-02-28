358ccfc20d41d143512b50a2c1c5bba6d49299de
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-02-28 20:30:44 +0530

    Update address.c

--- address.c
+++ address.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
     int main(){
-    int arr[5] = {4,5,6,7,8};
+    int arr[5] = {4,5,6,7,8,9};
     printf("%p\n",&arr[0]);
     printf("%p\n",&arr[1]);
     printf("%p\n",&arr[2]);
