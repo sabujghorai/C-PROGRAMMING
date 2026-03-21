9b0496be6016bf5992e1c9769527d38873a1469b
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-03-21 20:12:46 +0530

    implementing the pokemon's speed , hp and attack

--- structure.c
+++ structure.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
     int main(){
-    struct pokemon{
+    struct pokemon{ // pokemon is the user define data type
         int hp;
         int speed;
         int attack;
     };
+    struct pokemon pikachu;
+    pikachu.hp = 80;
+    pikachu.attack = 70;
+    pikachu.speed = 100;
+
+    
+printf("%d",pikachu.attack);
 return 0;
 }
