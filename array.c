97090c515fc435ef761b4d8393b5ceafdc700d86
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 21:41:02 +0530

    Taking input from user and print them reverse

--- array.c
+++ array.c
@@ -10,17 +10,33 @@
 // return 0;
 // }
 
+// #include<stdio.h>
+//     int main(){
+//     int arr[5];
+
+//     for(int i=0;i<5;i++){
+//         printf("Enter the elements :");
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("The elements are : ");
+//     for(int i=0; i<5;i++){
+//         printf(" %d",arr[i]);
+//     }
+// return 0;
+// }
+
+// Take input from user and print them in reverse order
 #include<stdio.h>
     int main(){
     int arr[5];
 
-    for(int i=0;i<5;i++){
-        printf("Enter the elements :");
+    for(int i=0; i<5; i++){
+        printf("Enter the elements : ");
         scanf("%d",&arr[i]);
     }
     printf("The elements are : ");
-   for(int i=0; i<5;i++){
-    printf(" %d",arr[i]);
-   }
+    for(int i=4; i>=0;i--){
+        printf("%d ",arr[i]);
+    }
 return 0;
 }
