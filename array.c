eb646eebe470304d4e5bbbcf0b53bdc47684b146
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 22:03:27 +0530

    updating the array file

--- array.c
+++ array.c
@@ -26,17 +26,38 @@
 // }
 
 // Take input from user and print them in reverse order
+// #include<stdio.h>
+//     int main(){
+//     int arr[5];
+
+//     for(int i=0; i<5; i++){
+//         printf("Enter the elements : ");
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("The elements are : ");
+//     for(int i=4; i>=0;i--){
+//         printf("%d ",arr[i]);
+//     }
+// return 0;
+// }
+
+
+// Given an array of marks of 5 students if the marks of any student is less then 35 then print their roll number 
+// [Roll number refers to the index of the array]
+
 #include<stdio.h>
     int main(){
-    int arr[5];
-
-    for(int i=0; i<5; i++){
-        printf("Enter the elements : ");
-        scanf("%d",&arr[i]);
+    int marks[5];
+    
+    for(int i=0;i<=4;i++){
+        printf("Enter the marks of the students :");
+        scanf("%d",&marks[i]);
     }
-    printf("The elements are : ");
-    for(int i=4; i>=0;i--){
-        printf("%d ",arr[i]);
-    }
+    printf("Roll number of failed students are : ");
+    for(int i=0; i<=4;i++){
+        if(marks[i]<35){
+             printf(" %d",i);
+        }
+    }  
 return 0;
 }
