24931c68ed85fd66e4183b66205ae84e2d7ee302
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 21:17:09 +0530

    taking user unput to print array

--- array.c
+++ array.c
@@ -1,11 +1,26 @@
+// #include<stdio.h>
+//     int main(){
+//         int arr[5] = {10,20,30,40,50};
+//         printf("The array elements are : ");
+
+//         for(int i=0; i <= 4;i++){
+//             printf(" %d",arr[i]);
+//         }
+
+// return 0;
+// }
+
 #include<stdio.h>
     int main(){
-        int arr[5] = {10,20,30,40,50};
-        printf("The array elements are : ");
+    int arr[10];
 
-        for(int i=0; i <= 4;i++){
-            printf(" %d",arr[i]);
-        }
-
+    for(int i=0;i<10;i++){
+        printf("Enter the elements :");
+        scanf("%d",&arr[i]);
+    }
+    printf("The elements are : ");
+   for(int i=0; i<10;i++){
+    printf(" %d",arr[i]);
+   }
 return 0;
 }
