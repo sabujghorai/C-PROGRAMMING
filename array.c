932ad785e164d3892bbc84266d97d2a0d5f52493
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 21:22:39 +0530

    correcting the for loop

--- array.c
+++ array.c
@@ -14,12 +14,12 @@
     int main(){
     int arr[5];
 
-    for(int i=1;i<=5;i++){
+    for(int i=0;i<5;i++){
         printf("Enter the elements :");
         scanf("%d",&arr[i]);
     }
     printf("The elements are : ");
-   for(int i=1; i<=5;i++){
+   for(int i=0; i<5;i++){
     printf(" %d",arr[i]);
    }
 return 0;
