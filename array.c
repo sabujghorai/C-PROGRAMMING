37fa0c638180bc10607cbd337cff6920f16553ce
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 20:56:14 +0530

    updating the array file

--- array.c
+++ array.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
     int main(){
         int arr[5] = {10,20,30,40,50};
-        printf("The array elements are :");
-        
+        printf("The array elements are : ");
+
         for(int i=0; i <= 4;i++){
             printf(" %d",arr[i]);
         }
