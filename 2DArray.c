34d8343470ff10e89a538441b541b18c04a9588d
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-11 19:53:30 +0530

    updated the 2Darray file

--- 2DArray.c
+++ 2DArray.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
     int main(){
-    int arr[2][3] = {{1,2},{3,4},{5,6}};
+    int arr[2][2] = {{1,2},{3,4}};
     for(int i=0;i<=1;i++){
-        for(int j=0;j<=2;j++){
-            printf("%d\n",a[i][j]);
+        for(int j=0;j<=1;j++){
+            printf("%d ",arr[i][j]);
         }
-        printf("%d");
+        printf("\n");
     }
 return 0;
 }
