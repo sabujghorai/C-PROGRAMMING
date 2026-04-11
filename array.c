3343a5d28d2ab4b9ddf8a23e7f5721c1abb578f2
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-11 10:43:09 +0530

    updated the array.c file

--- array.c
+++ array.c
@@ -1,75 +1,92 @@
+// #include<stdio.h>
+//     int main(){
+//         int arr[5] = {10,20,30,40,50};
+//         printf("The array elements are : ");
+
+//         for(int i=0; i <= 4;i++){
+//             printf(" %d",arr[i]);
+//         }
+
+// return 0;
+// }
+
+// #include<stdio.h>
+//     int main(){
+//     int arr[5];
+
+//     for(int i=0;i<5;i++){
+//         printf("Enter the elements :");
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("The elements are : ");
+//     for(int i=0; i<5;i++){
+//         printf(" %d",arr[i]);
+//     }
+// return 0;
+// }
+
+// Take input from user and print them in reverse order
+// #include<stdio.h>
+//     int main(){
+//     int arr[5];
+
+//     for(int i=0; i<5; i++){
+//         printf("Enter the elements : ");
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("The elements are : ");
+//     for(int i=4; i>=0;i--){
+//         printf("%d ",arr[i]);
+//     }
+// return 0;
+// }
+
+
+// // Given an array of marks of 5 students if the marks of any student is less then 35 then print their roll number 
+// // [Roll number refers to the index of the array]
+
+// #include<stdio.h>
+//     int main(){
+//     int marks[5];
+//     for(int i=0;i<=4;i++){
+//         printf("Enter the marks of the students :");
+//         scanf("%d",&marks[i]);
+//     }
+//     printf("Roll number of failed students are : ");
+//     for(int i=0; i<=4;i++){
+//         if(marks[i]<35){
+//              printf(" %d",i);
+//         }
+//     }  
+// return 0;
+// }
+
+// // Calculate the product of all the elements in given array
+
+// #include<stdio.h>
+//     int main(){
+//     int arr_product[5] = {1,2,3,4,5};
+//     int product = 1;
+//     for(int i=0;i<=4;i++){
+//         product = product*arr_product[i];
+//     }
+//     printf("The product of all the elements is : %d ",product);
+// return 0;
+// }
+
+
+// Find the maximum value out of all the elements in array
+
 #include<stdio.h>
     int main(){
-        int arr[5] = {10,20,30,40,50};
-        printf("The array elements are : ");
-
-        for(int i=0; i <= 4;i++){
-            printf(" %d",arr[i]);
+    int max_value[6] ={3,4,5,6,7,9};
+    int max = -1;
+    for(int i=0;i<=5;i++){
+        if(max<max_value[i]){
+            max = max_value[i];
         }
-
-return 0;
-}
-
-#include<stdio.h>
-    int main(){
-    int arr[5];
-
-    for(int i=0;i<5;i++){
-        printf("Enter the elements :");
-        scanf("%d",&arr[i]);
     }
-    printf("The elements are : ");
-    for(int i=0; i<5;i++){
-        printf(" %d",arr[i]);
-    }
-return 0;
-}
+    printf("The maximum value is : %d",max);
 
-Take input from user and print them in reverse order
-#include<stdio.h>
-    int main(){
-    int arr[5];
-
-    for(int i=0; i<5; i++){
-        printf("Enter the elements : ");
-        scanf("%d",&arr[i]);
-    }
-    printf("The elements are : ");
-    for(int i=4; i>=0;i--){
-        printf("%d ",arr[i]);
-    }
-return 0;
-}
-
-
-// Given an array of marks of 5 students if the marks of any student is less then 35 then print their roll number 
-// [Roll number refers to the index of the array]
-
-#include<stdio.h>
-    int main(){
-    int marks[5];
-    for(int i=0;i<=4;i++){
-        printf("Enter the marks of the students :");
-        scanf("%d",&marks[i]);
-    }
-    printf("Roll number of failed students are : ");
-    for(int i=0; i<=4;i++){
-        if(marks[i]<35){
-             printf(" %d",i);
-        }
-    }  
-return 0;
-}
-
-// Calculate the product of all the elements in given array
-
-#include<stdio.h>
-    int main(){
-    int arr_product[5] = {1,2,3,4,5};
-    int product = 1;
-    for(int i=0;i<=4;i++){
-        product = product*arr_product[i];
-    }
-    printf("The product of all the elements is : %d ",product);
 return 0;
 }
