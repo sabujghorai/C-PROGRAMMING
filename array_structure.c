c71b94f3e18f091b3a13697eeea7c74052c26a71
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-04-18 00:04:32 +0530

    Update array_structure.c

--- array_structure.c
+++ array_structure.c
@@ -11,7 +11,6 @@ int main() {
         printf("Enter roll and marks for student %d: ", i+1);
         scanf("%d %f", &s[i].roll, &s[i].marks);
     }
-
     printf("\nStudent Details:\n");
     for(int i = 0; i < 3; i++) {
         printf("Roll: %d, Marks: %.2f\n", s[i].roll, s[i].marks);
