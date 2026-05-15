afe11235eb5a055cd6a9a2036c4775aa539f239f
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-15 10:43:00 +0530

    updated the temporary code runner file

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-    int main(){
-    for(int i=1;i<=10;i++){
-        printf("%d\n",i);
+int main(){
+    for(int i=0;i<=15;i++){
+        printf("%d \n",i);
     }
 return 0;
-    }
+}
