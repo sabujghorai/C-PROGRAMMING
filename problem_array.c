38ef44fbc9f200809c606dfeb80ef2bc7581d03f
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-16 23:30:16 +0530

    updated the array problem file

--- problem_array.c
+++ problem_array.c
@@ -1,19 +1,19 @@
-// Take 5 input from user and print them in reverse....
+Take 5 input from user and print them in reverse....
 
-// #include<stdio.h>
-//     int main(){
-//     int arr[5];
+#include<stdio.h>
+    int main(){
+    int arr[5];
 
-//     for(int i = 0; i<5; i++){
-//         printf("Enter element no %d :",i+1);
-//         scanf("%d",&arr[i]);
-//     }
+    for(int i = 0; i<5; i++){
+        printf("Enter element no %d :",i+1);
+        scanf("%d",&arr[i]);
+    }
 
-//     for(int j=4 ; j>=0 ; j--){
-//         printf("%d \n",arr[j]);
-//     }
-// return 0;
-// }
+    for(int j=4 ; j>=0 ; j--){
+        printf("%d \n",arr[j]);
+    }
+return 0;
+}
 
 
 /*  Given an array of marks of students. if the mark of student is less than 35
