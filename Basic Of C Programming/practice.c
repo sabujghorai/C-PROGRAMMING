071f8554644aed81bf2fb1d309b3ebf28ab12235
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-19 23:33:20 +0530

    updated the c file

--- Basic Of C Programming/practice.c
+++ Basic Of C Programming/practice.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
-  int main(){
-  int a,b;
+#include <stdio.h>
+int main()
+{
+  int a, b;
   printf("Enter the length of the rectangle :");
-  scanf("%d",&a);
+  scanf("%d", &a);
   printf("Enter the bredth of the rectangle :");
-  scanf("%d",&b);
+  scanf("%d", &b);
 
-  printf("the area of the rectangle is : %d",a*b);
-return 0;
+  printf("the area of the rectangle is : %d", a * b);
+  return 0;
 }
 // The area of the area....
-#include<stdio.h>
-  int main(){
+#include <stdio.h>
+int main()
+{
   int a;
   printf("Enter the sid :");
-  scanf("%d",&a);
+  scanf("%d", &a);
 
-  printf("The area of the squre is = %d",a*a );
+  printf("The area of the squre is = %d", a * a);
 
-return 0;
+  return 0;
 }
