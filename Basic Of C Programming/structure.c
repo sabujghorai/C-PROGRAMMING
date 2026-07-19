d6396d7b2c7669b8428c3f216f6c2dfa6a7af783
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-19 23:34:06 +0530

    updated the c file

--- Basic Of C Programming/structure.c
+++ Basic Of C Programming/structure.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
-    int main(){
-    struct pokemon{ // pokemon is the user define data type
+#include <stdio.h>
+int main()
+{
+    struct pokemon
+    { // pokemon is the user define data type
         int hp;
         int speed;
         int attack;
     };
     struct pokemon pikachu;
     printf("Enter the attack of pikachu :");
-    scanf("%d",&pikachu.attack);
+    scanf("%d", &pikachu.attack);
     pikachu.hp = 80;
     // pikachu.attack = 70;
     pikachu.speed = 100;
 
-    struct pokemon charizard ;
+    struct pokemon charizard;
     charizard.hp = 120;
     charizard.attack = 90;
     charizard.speed = 200;
 
-printf(" Attack of pikachu stored is = %d \n",pikachu.attack);
-return 0;
+    printf(" Attack of pikachu stored is = %d \n", pikachu.attack);
+    return 0;
 }
